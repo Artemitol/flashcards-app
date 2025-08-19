4cabc8bce5bf8bc2d3fc18ffd806ddaa00efb656
Artemitol <artemiy.averin@gmail.com>
2025-08-19 21:31:09 +0300

    refactor: removed logic to the service from quiz-by-id.server

--- src/features/quiz/get-by-id/ui/quiz-by-id.server.tsx
+++ src/features/quiz/get-by-id/ui/quiz-by-id.server.tsx
@@ -1,18 +1,13 @@
-import { getQuizzesService } from "@entities/quiz/server"
-import { notFound } from "next/navigation"
 import { QuizById } from "./quiz-by-id"
 import { QuizByIdParams } from "../model/params-domain"
 import cl from "./quiz-by-id.server.module.scss"
 import { Typography } from "@shared/ui/typography"
+import { getQuizByIdService } from "../services/get-quiz-by-id"
 
 export async function QuizByIdServer({ params }: { params: QuizByIdParams }) {
     const { id } = await params
 
-    const quiz = await getQuizzesService.getById(Number(id))
-
-    if (quiz.type === "left" && quiz.error.message === "no-such-quiz") {
-        notFound()
-    }
+    const quiz = await getQuizByIdService(Number(id))
 
     if (quiz.type === "left") {
         return <div>something went wrong...</div>
