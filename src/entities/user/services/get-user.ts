85ebb6ddfd6e7223f4f48df7417776f9c1382ff5
Artemitol <artemiy.averin@gmail.com>
2025-08-26 10:09:58 +0300

    feat: added caching for user services

--- src/entities/user/services/get-user.ts
+++ src/entities/user/services/get-user.ts
@@ -5,28 +5,45 @@ import { userRepository } from "../repositories/user-repository"
 import { users } from "@shared/model/db/server"
 import { UserId } from "@kernel/ids"
 import { left, right } from "@shared/lib/either"
+import { unstable_cache } from "next/cache"
 
-const getUserById = async (id: UserId) => {
-    const queryResult = await userRepository.getUser(eq(users.id, id))
+const getUserById = (id: UserId) =>
+    unstable_cache(
+        async () => {
+            const queryResult = await userRepository.getUser(eq(users.id, id))
 
-    if (queryResult) {
-        return right(queryResult)
-    }
+            if (queryResult) {
+                return right(queryResult)
+            }
 
-    return left("no-such-user")
-}
+            return left("no-such-user")
+        },
+        [id.toString()],
+        {
+            revalidate: 60,
+            tags: ["user", "byId"],
+        }
+    )()
 
-const getUserByUsername = async (username: string) => {
-    const queryResult = await userRepository.getUser(
-        eq(users.username, username)
-    )
+const getUserByUsername = (username: string) =>
+    unstable_cache(
+        async () => {
+            const queryResult = await userRepository.getUser(
+                eq(users.username, username)
+            )
 
-    if (queryResult) {
-        return right(queryResult)
-    }
+            if (queryResult) {
+                return right(queryResult)
+            }
 
-    return left("no-such-user")
-}
+            return left("no-such-user")
+        },
+        [username.toString()],
+        {
+            revalidate: 60,
+            tags: ["user", "byUsername"],
+        }
+    )()
 
 export const getUserService = {
     byId: getUserById,
