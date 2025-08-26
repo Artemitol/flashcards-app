2ffb7cc695b047fd80d70e857d47b4695ae73f65
Artemitol <artemiy.averin@gmail.com>
2025-08-26 09:56:10 +0300

    refactor: deleted unused question slice

--- src/entities/question/index.ts
+++ src/entities/question/index.ts
@@ -1,10 +1,3 @@
 export { QuestionFlipCard } from "./ui/question-flip-card"
 
 export type { QuestionModel } from "./model/domain/question"
-export {
-    questionActions,
-    questionSelectors,
-    questionsSlice,
-} from "./model/questions.slice"
-
-export { useQuestionCards } from "./lib/use-question-cards"
